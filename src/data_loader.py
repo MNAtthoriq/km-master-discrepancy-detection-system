bd35a4e12947bf70285a2462d2cbca6b6f8d3e0a
MN Atthoriq <mnatthoriq@gmail.com>
2025-11-06 15:49:43 +0700

    Update data_loader.py
    Change return variable from worksheet to sheet for avoiding ambiguos name

--- src/data_loader.py
+++ src/data_loader.py
@@ -21,9 +21,9 @@ def sheets_loader(sheet_url: str, credentials_path: str=os.path.join(os.path.dir
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
     creds = Credentials.from_service_account_file(credentials_path, scopes=scopes)
     client = gspread.authorize(creds)
-    worksheet = client.open_by_url(sheet_url)
+    sheet = client.open_by_url(sheet_url)
     
-    return worksheet
+    return sheet
 
 if __name__ == "__main__":
     # back to project root
