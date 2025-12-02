bfa5318e66b9f0969c0379169dfb4a6adf857fea
MNAtthoriq <mnatthoriq@gmail.com>
2025-12-03 03:57:43 +0700

    add logger and __init__.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,6 +6,15 @@ import gspread
 from google.oauth2.service_account import Credentials
 import os
 from dotenv import load_dotenv
+import logging
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()]
+    )
+logger = logging.getLogger(__name__)
 
 def sheets_loader(sheet_url: str, credentials_path: str=os.path.join(os.path.dirname(__file__), "..", "credentials.json")):
     """
@@ -18,12 +27,21 @@ def sheets_loader(sheet_url: str, credentials_path: str=os.path.join(os.path.dir
     Returns:
     gspread.Spreadsheet: Google Sheet
     """
+    logger.info(f"Loading Google Sheets from URL: {sheet_url[:30]}...")
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+
     creds = Credentials.from_service_account_file(credentials_path, scopes=scopes)
+    logger.info("Credentials loaded successfully")
+
     client = gspread.authorize(creds)
+    logger.info("Google Sheets API authorized")
+
     sheet = client.open_by_url(sheet_url)
+    logger.info("Sheet opened successfully")
     
     return sheet
+
+# !notes: add sheets_updater function later
 
 if __name__ == "__main__":
     # back to project root
