a974b72f2a5d770fd366a3e6a2a7116f5334778b
MN Atthoriq <mnatthoriq@gmail.com>
2025-12-12 17:26:19 +0700

    Update IQR_COLUMN

--- src/config.py
+++ src/config.py
@@ -50,7 +50,7 @@ CSN_MASTER_TOKO_BENAR = "Toko Benar"
 # UTILS CONFIGURATION
 # ==============================================================
 SYMBOLS_TO_PRESERVE = (',', '.', '%', ' ', '+', '-')
-IQR_COLUMN = "KM Deviation (%)"
+IQR_COLUMN = "KM Deviation"
 LOWER_IQR_CONSTANT = 1.5
 UPPER_IQR_CONSTANT = 1.5
 LOWER_ZOOM_CONSTANT = 10.0
